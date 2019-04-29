304f7325a0eaca87bd487a090936c83b23e04fe0
Ashwani Arya <ashwaniparker@gmail.com>
2019-04-29 23:36:07 +0530

    Change env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {typeDefs,resolvers} = require('./src/resources')
 
 // Connect ot mongodb
 mongoose.set('bufferCommands', false);
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true });
 
 const server = new ApolloServer({
   typeDefs,
